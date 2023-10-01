1221c5fb1a369e50476da36c2c60c500595fa240
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-10-01 15:23:10 +0200

    added useEffect to movies

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,47 +1,63 @@
-import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect} from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { fetchMovies } from 'services/api';
+import { useSearchParams } from 'react-router-dom';
 
-const headers = {
-  Authorization:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjljNGQ1OGI3Nzc5Y2E3ZjU0NzQzOGVjMDY1YTdkMiIsInN1YiI6IjYzYWRlYjM2MWY3NDhiMDBjOGI3NTk3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yuBGdZBeGbe4GxiKZv6q5aaLChprjgO_x8cJYcfPPzA',
-  accept: 'application/json',
-};
-async function fetchMovies(query) {
-  try {
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, { headers });
-    const data = await response.json()
-     return data.results;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
 function Movies() {
-  const [queryResults, setQueryResults] = useState(false);
+  const [queryResults, setQueryResults] = useState(false); //false
   const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryParam = searchParams.get('query');
 
-  const handleSubmit = async (e) => {
+  console.log("queryResults", queryResults);
+  console.log("queryParam", queryParam);
+
+ useEffect(() => {
+
+   if (queryParam) {
+     fetchMovies(queryParam)
+       .then(data => {
+         setQueryResults(data);
+       })
+       .catch(error => {
+         console.error('Error fetching movies:', error);
+       });
+   } else {
+     // Jeśli brak parametru zapytania, ustaw listę wyników na pustą
+     setQueryResults(false);
+   }
+ }, [queryParam]);
+  const handleSubmit = async e => {
     e.preventDefault();
     const form = e.currentTarget;
     const input = form.elements.searchQuery.value;
-    const data = await fetchMovies(input);
-    setQueryResults(data)
-  }
+    const data = await fetchMovies(queryParam || input);
+    setQueryResults(data);
+    setSearchParams({ query: input });
+  };
 
-    return (
-      <>
-        <h1>Movies</h1>
-        <form onSubmit={handleSubmit}>
-          <input name="searchQuery"></input>
-          <button>Search</button>
-        </form>
-       
-        <ul>{queryResults ? ( queryResults.map(movie => (
-          <li key={movie.id}><Link to={`${movie.id}`} state={{ from: location }}>{movie.title}</Link></li>
-        ))) : (null)}</ul>
-        {queryResults.length === 0 && <p>No results found :(</p>}
-      </>
-    );
+  return (
+    <>
+      <h1>Movies</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="searchQuery"></input>
+        <button>Search</button>
+      </form>
+
+      <ul>
+        {queryResults.length > 0
+          ? queryResults.map(movie => (
+              <li key={movie.id}>
+                <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+                  {movie.title}
+                </Link>
+              </li>
+            ))
+          : null}
+      </ul>
+      {queryResults.length === 0 && <p>No results found :(</p>}
+    </>
+  );
 }
 
 export default Movies;
