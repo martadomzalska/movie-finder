c929a1b98bddac477c82a4988754b0f474ccc2a1
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2024-10-15 22:18:56 +0200

    update basename in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
- 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter basename="/movie-finder/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
