b8ad0bc2edfc28bd18f5e20c9276351e1f454438
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2024-11-23 22:43:30 +0100

    updated MovieDetails

---
+++ src/pages/Moviedetails/MovieDetails.js
@@ -0,0 +1,93 @@
+import { useParams, useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { getMovieDetails } from 'services/api';
+import { Link, Outlet } from 'react-router-dom';
+import css from './MovieDetails.module.css';
+import { ReactComponent as GoBackBtn } from './go-back-button.svg';
+import { Helmet } from 'react-helmet';
+
+function MovieDetails() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({}); //''
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? '/movies';
+
+  useEffect(() => {
+    async function fetchMovieDetails() {
+      const data = await getMovieDetails(id);
+      setMovie({
+        id: data.id,
+        title: data.title,
+        overview: data.overview,
+        userScore: data.popularity,
+        genres: data.genres,
+        poster_path: data.poster_path,
+      });
+    }
+
+    fetchMovieDetails();
+  }, [id]);
+
+  const genres =
+    movie && movie.genres && movie.genres.map(genre => genre.name).join(', ');
+
+  console.log(movie);
+  return (
+    <div className={css.container}>
+      <Helmet>
+        <title>{`🔎 ${movie.title} • Details`}</title>
+      </Helmet>
+      <Link className={css.goBackLink} to={backLinkHref}>
+        <GoBackBtn className={css.goBackBtn} />
+      </Link>
+
+      <div className={css.section1}>
+        {movie &&
+          movie.poster_path && ( //image
+            <img
+              className={css.image}
+              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+              alt="Movie poster"
+              width="100%"
+            />
+          )}
+
+        <div className={css.movieInfo}>
+          <h1 className={css.movieTitle}>{movie.title}</h1>
+          <p>User score: {movie.userScore}%</p>
+          <h3>Overview:</h3>
+          <p className={css.movieOverview}>{movie.overview}</p>
+          <h4>
+            Genres:
+            <div className={css.movieGenre}>{genres}.</div>
+          </h4>
+        </div>
+      </div>
+      <div className={css.additionalInfo}>
+        <h4 className={css.additionalInfo_headline}>Additional information:</h4>
+        <ul className={css.additionalInfoList}>
+          <li>
+            <Link
+              className={css.additionalInfoLink}
+              to="cast"
+              state={{ id: id, from: backLinkHref }}
+            >
+              Cast
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={css.additionalInfoLink}
+              to="reviews"
+              state={{ id: id, from: backLinkHref }}
+            >
+              Reviews
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+export default MovieDetails;
