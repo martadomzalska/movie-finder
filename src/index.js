ab28e7c191c2348b93b59c5218aa58d833e14ecb
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2024-11-23 22:43:53 +0100

    fixed basename in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/movie-finder/">
+    <BrowserRouter basename="/movie-finder">
       <App />
     </BrowserRouter>
   </React.StrictMode>
